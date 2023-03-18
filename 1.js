f54aaca23535dfcfcb907852682d742d4eb0dc91
lordbaldwin1 <116612584+lordbaldwin1@users.noreply.github.com>
2023-03-17 19:37:23 -0700

    added burger menu and scroll to top of page js

---
+++ 1.js
@@ -0,0 +1,19 @@
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
