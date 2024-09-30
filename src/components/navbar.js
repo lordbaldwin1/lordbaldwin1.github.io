b644717d103a62d54dbe0497c7870be168e0f557
Zachary Springer <springerzman@gmail.com>
2024-09-29 18:11:10 -0700

    add about/resume

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Resume from '../assets/Resume_Zachary_Springer.pdf';
 
 function Navigation() {
   return (
@@ -12,6 +13,7 @@ function Navigation() {
             <Nav.Link as={Link} to="/projects" className="hover-effect">Projects</Nav.Link>
             <Nav.Link as={Link} to="/about" className="hover-effect">About</Nav.Link>
             <Nav.Link as={Link} to="/contact" className="hover-effect">Contact</Nav.Link>
+            <Nav.Link href={Resume} download="Resume_Zachary_Springer.pdf" className="hover-effect">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
--- src/pages/About.js
+++ src/pages/About.js
@@ -13,12 +13,28 @@ const About = () => {
       >
         <h1>About Me</h1>
         <p>
-          I am a passionate web developer with a strong background in building dynamic and responsive web applications. I enjoy creating user-friendly experiences and continuously learning new technologies to improve my skill set.
+          Hello! I'm a recent graduate from Portland State University with a
+          Bachelor of Science in Computer Science as of August 2024. I've lived in Portland
+          my entire life and love the outdoors and working out. The gym has become a second
+          home for me. Without missing more than a couple of days in a row, I've worked out at the gym
+          consistently for the last 2.5 years, something I had tried to get into and failed quite
+          a few times. More than anything, this has taught me that to be great at any activity or skill in life, you must
+          dedicate yourself to your work and remain consistent. <br /> <br />
+
+          I've also had a love for computers my entire life, especially video games. Video games
+          are where I've met many of my closest friends and also learned a couple very important skills.
+          Firstly, through competitive gaming, I've learned how to learn. By this, I mean that I've learned how to
+          pick up new skills quickly and apply them to my current repertoire. I've also learned the importance of mindfulness
+          in my own work and striving to always improve. This applies directly to programming where with every piece of code written,
+          I try to think about my faults and areas to improve upon. If it's not clear what I'm to improve upon, then that usually
+          means, for me, that it's time to study the work of others to improve my own. <br /> <br />
+
+          Also, again as with the gym, competitive video
+          games have taught me the importance of consistency. To be great
+          in any competitive video game, you need to not only play consistently,
+          but you need to be mindful and play to improve consistently. Many people go through work and life on autopilot, but to keep improving
+          and learning, you need to keep striving to do so.
         </p>
-        <p>
-          My journey began with a deep interest in technology, and over the years, I have developed a diverse set of skills that span both front-end and back-end development. I am committed to delivering high-quality code and bringing creative ideas to life.
-        </p>
-        {/* Add more personal details or background information here */}
     </motion.section>
   );
 };
